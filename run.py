fbe7ae799653263458805e17faceed5eebdf130a
RobWar-code <rowaniar@aol.com>
2022-11-28 19:05:41 +0000

    Add update stock

--- run.py
+++ run.py
@@ -94,7 +94,24 @@ def get_last_five_entries_sales():
     for ind in range(1, 7):
         column = sales.col_values(ind)
         columns.append(column[-5:])
-    pprint(columns)
+
+    return columns
+
+
+def calculate_stock_data(data):
+    """
+        Determine the averages and recommended stock levels (+10%)
+    """
+    print("Calculating stock levels...\n")
+
+    new_stock_data = []
+    for col in data:
+        int_col = [int(num) for num in col]
+        average = sum(int_col)/len(int_col)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
 
 
 def main():
@@ -106,8 +123,10 @@ def main():
     update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
+    sales_columns = get_last_five_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 
 print("Welcome to Love Sandwiches Stock Automation\n")
-# main()
-get_last_five_entries_sales()
+main()
